6b1175c2b916253ee690273700b784728133175d
David Stone <david_stone@intuit.com>
2017-08-13 18:29:52 -0700

    Remove my implementation

--- src/main/java/website/sdfp/textadventure/Game.java
+++ src/main/java/website/sdfp/textadventure/Game.java
@@ -1,40 +1,27 @@
 package website.sdfp.textadventure;
 
-import org.immutables.value.Value;
 import website.sdfp.textadventure.library.GameLoop;
 import website.sdfp.textadventure.library.GameState;
 
-import java.util.Random;
-
-import static website.sdfp.textadventure.library.GameStateUtils.*;
+import static website.sdfp.textadventure.library.GameStateUtils.end;
+import static website.sdfp.textadventure.library.GameStateUtils.next;
 
 class Game {
-    @Value.Immutable
-    public interface GameData {
-        int dice();
+    static class GameData {
     }
 
-    private static Random random = new Random();
 
-    static GameState logic(ImmutableGameData currentData, String userInput) {
+    static GameState logic(GameData currentData, String userInput) {
         switch(userInput) {
             case "":
-                return next("you just hit enter...what do you want?", currentData);
-            case "roll":
-                int newDice = random.nextInt(20);
-                ImmutableGameData newGameData = currentData.withDice(newDice);
-                return next("You rolled a " + newGameData.dice() + ". Now what?", newGameData);
+                return next("You just hit enter...what do you want?", currentData);
         }
 
-        return end("that's not a valid option");
+        return end("That's not a valid option");
     }
 
     public static void main(String[] args) throws Exception {
-        ImmutableGameData gameData = ImmutableGameData
-                .builder()
-                .dice(10)
-                .build();
-
+        GameData gameData = new GameData();
         GameLoop.run(gameData, "Hi!", Game::logic);
     }
 }
